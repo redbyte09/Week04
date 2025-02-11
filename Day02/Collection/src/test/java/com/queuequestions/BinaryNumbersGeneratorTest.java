c7a81759b8c2c45deee74b557b2e64cf29bda0d5
redbyte09 <sakshisharma4234@gmail.com>
2025-02-11 22:19:41 +0530

    Testing of  Binary Numbers Generator question

--- Day02/Collection/src/test/java/com/queuequestions/BinaryNumbersGeneratorTest.java
+++ Day02/Collection/src/test/java/com/queuequestions/BinaryNumbersGeneratorTest.java
@@ -1,4 +1,13 @@
 package com.queuequestions;
 
-public class BinaryNumbersGeneratorTest {
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryNumbersGeneratorTest {
+
+    @Test
+    void testGenerateBinaryNumbers() {
+        assertEquals(List.of("1", "10", "11", "100", "101"), BinaryNumbersGenerator.generateBinaryNumbers(5));
+    }
 }
