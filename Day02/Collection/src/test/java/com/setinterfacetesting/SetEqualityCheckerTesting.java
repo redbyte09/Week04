25ed6e56421748d3b350d4bffd14a723feb66f37
redbyte09 <sakshisharma4234@gmail.com>
2025-02-11 16:19:27 +0530

    Testing of question is sets are equal or not

--- Day02/Collection/src/test/java/com/setinterfacetesting/SetEqualityCheckerTesting.java
+++ Day02/Collection/src/test/java/com/setinterfacetesting/SetEqualityCheckerTesting.java
@@ -1,4 +1,25 @@
 package com.setinterfacetesting;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetEqualityCheckerTesting {
+
+    @Test
+    public void testEquals(){
+        Set<Integer> set1=new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        Set<Integer> set2=new HashSet<>();
+        set2.add(3);
+        set2.add(2);
+        set2.add(1);
+
+        Assertions.assertTrue(set1.equals(set2));
+    }
 }
