4c453c49c5c6d43e058befc5415954ac08d30787
redbyte09 <sakshisharma4234@gmail.com>
2025-02-11 16:01:18 +0530

    Testing for  Finding the Nth element from end question

--- Day02/Collection/src/test/java/com/listquestiontesting/NthElementFromEndTesting.java
+++ Day02/Collection/src/test/java/com/listquestiontesting/NthElementFromEndTesting.java
@@ -1,4 +1,25 @@
 package com.listquestiontesting;
 
+import com.listquestions.NthElementFromEnd;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
 public class NthElementFromEndTesting {
+
+    @Test
+        public void testFindNthFromEnd_ValidCase() {
+            NthElementFromEnd<String> finder = new NthElementFromEnd<>();
+            LinkedList<String> list = new LinkedList<>();
+            list.add("A");
+            list.add("B");
+            list.add("C");
+            list.add("D");
+            list.add("E");
+
+            String result = finder.findNthFromEnd(list, 2);
+            Assertions.assertEquals("D", result);
+        }
+
 }
