bdfb41949c969e599a064b9ac5fa40d147eee9c8
redbyte09 <sakshisharma4234@gmail.com>
2025-02-11 14:07:53 +0530

    This is testing file of arraylist

--- Day02/Collection/src/test/java/com/listquestiontesting/ReverseByArrayListTesting.java
+++ Day02/Collection/src/test/java/com/listquestiontesting/ReverseByArrayListTesting.java
@@ -1,4 +1,33 @@
 package com.listquestiontesting;
 
+import com.listquestions.ReverseByArrayList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReverseByArrayListTesting {
+    @Test
+    void test1() {
+        ReverseByArrayList<Integer> r = new ReverseByArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(4);
+        list.add(3);
+        list.add(2);
+        list.add(1);
+
+        ArrayList<Integer> reversedList = r.reverse(list);
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        arr2.add(1);
+        arr2.add(2);
+        arr2.add(3);
+        arr2.add(4);
+        arr2.add(5);
+
+        Assertions.assertEquals(reversedList,arr2);
+
+//        Assertions.assertEquals(42, result, "The result should be 42");
+    }
 }
