737dd5cab70459a4338fe1b11ad170f158c47b30
redbyte09 <sakshisharma4234@gmail.com>
2025-02-11 15:46:25 +0530

    Testing of Remove Duplicates question

--- Day02/Collection/src/test/java/com/listquestiontesting/RemoveDuplicatesTesting.java
+++ Day02/Collection/src/test/java/com/listquestiontesting/RemoveDuplicatesTesting.java
@@ -1,4 +1,20 @@
 package com.listquestiontesting;
 
+import com.listquestions.RemoveDuplicates;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class RemoveDuplicatesTesting {
+
+    @Test
+    public void testRemoveDuplicates() {
+        RemoveDuplicates <Integer>removeDuplicates = new RemoveDuplicates();
+        List<Integer> input = Arrays.asList(3, 1, 2, 2, 3, 4);
+        List<Integer> expected = Arrays.asList(3, 1, 2, 4);
+        List<Integer> result = removeDuplicates.removeDuplicates(input);
+        Assertions.assertEquals(expected, result);
+    }
 }
