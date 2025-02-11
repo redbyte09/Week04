ac87df9f4a3c5e601b4e78852bec7342a46fcfc6
redbyte09 <sakshisharma4234@gmail.com>
2025-02-11 22:05:06 +0530

    Testing of  Reverse queue question

--- Day02/Collection/src/test/java/com/queuequestions/ReverseQueueTest.java
+++ Day02/Collection/src/test/java/com/queuequestions/ReverseQueueTest.java
@@ -1,4 +1,27 @@
-package com.queuequestions;
+package com.queueoperations;
 
-public class ReverseQueueTest {
+import com.queuequestions.ReverseQueue;
+import org.junit.jupiter.api.Test;
+import java.util.LinkedList;
+import java.util.Queue;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseQueueTest {
+
+    @Test
+    void testReverseQueue() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(10);
+        queue.offer(20);
+        queue.offer(30);
+
+        Queue<Integer> expectedReversedQueue = new LinkedList<>();
+        expectedReversedQueue.offer(30);
+        expectedReversedQueue.offer(20);
+        expectedReversedQueue.offer(10);
+
+        Queue<Integer> actualReversedQueue = ReverseQueue.reverseQueue(queue);
+
+        assertEquals(expectedReversedQueue, actualReversedQueue);
+    }
 }
