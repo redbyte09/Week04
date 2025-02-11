8d686850fea81ad704ea084f76faee222d1d55dc
redbyte09 <sakshisharma4234@gmail.com>
2025-02-11 15:18:18 +0530

    Testing of Frequency counter question

--- Day02/Collection/src/test/java/com/listquestiontesting/FrequencyCounterTesting.java
+++ Day02/Collection/src/test/java/com/listquestiontesting/FrequencyCounterTesting.java
@@ -1,4 +1,28 @@
 package com.listquestiontesting;
 
+import com.listquestions.FrequencyCounter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class FrequencyCounterTesting {
+
+    @Test
+    public void Test1(){
+        FrequencyCounter<String>counter=new FrequencyCounter<>();
+        List<String> words = Arrays.asList("apple", "banana", "apple", "orange");
+
+        Map<String ,Integer> expectedOutput = new HashMap<>();
+        expectedOutput.put("apple",2);
+        expectedOutput.put("banana",1);
+        expectedOutput.put("orange",1);
+
+        Map<String, Integer> result = counter.countFrequency(words);
+        Assertions.assertEquals(expectedOutput,result);
+
+    }
 }
