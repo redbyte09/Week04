64b877f00079fbcc182d823217f4387b2d7d045c
redbyte09 <sakshisharma4234@gmail.com>
2025-02-11 15:35:01 +0530

    Testing of  List Rotator question

--- Day02/Collection/src/test/java/com/listquestiontesting/ListRotatorTesting.java
+++ Day02/Collection/src/test/java/com/listquestiontesting/ListRotatorTesting.java
@@ -1,4 +1,22 @@
 package com.listquestiontesting;
 
+import com.listquestions.ListRotator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class ListRotatorTesting {
+
+    @Test
+    public void testRotationByTwoPositions() {
+        ListRotator<Integer> rotator = new ListRotator<>();
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+        List<Integer> expected = Arrays.asList(30, 40, 50, 10, 20);
+
+        List<Integer> result = rotator.rotate(numbers, 2);
+
+        Assertions.assertEquals(expected, result);
+    }
 }
