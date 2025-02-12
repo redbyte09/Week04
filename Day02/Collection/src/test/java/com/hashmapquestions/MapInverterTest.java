16a4d725ec1c4a72176193afa3fb470b10bd1797
redbyte09 <sakshisharma4234@gmail.com>
2025-02-12 09:48:09 +0530

    Testing of Map Inverter question

--- Day02/Collection/src/test/java/com/hashmapquestions/MapInverterTest.java
+++ Day02/Collection/src/test/java/com/hashmapquestions/MapInverterTest.java
@@ -1,4 +1,33 @@
 package com.hashmapquestions;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+
 public class MapInverterTest {
+
+    @Test
+    public void testInvertMap_BasicCase() {
+        Map<String, Integer> inputMap = new HashMap<>();
+        inputMap.put("A", 1);
+        inputMap.put("B", 2);
+        inputMap.put("C", 1);
+
+        Map<Integer, List<String>> expected = new HashMap<>();
+        expected.put(1, Arrays.asList("A", "C"));
+        expected.put(2, Collections.singletonList("B"));
+
+        assertEquals(expected, MapInverter.invertMap(inputMap));
+    }
+
+    @Test
+    public void testInvertMap_SingleEntry() {
+        Map<String, Integer> inputMap = new HashMap<>();
+        inputMap.put("X", 5);
+
+        Map<Integer, List<String>> expected = new HashMap<>();
+        expected.put(5, Collections.singletonList("X"));
+
+        assertEquals(expected, MapInverter.invertMap(inputMap));
+    }
 }
