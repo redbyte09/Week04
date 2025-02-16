75a072a3013270ad819f9a2cf34b17bb8ab60b6f
redbyte09 <sakshisharma4234@gmail.com>
2025-02-16 19:57:25 +0530

    [Sakshi] solved get class information question

--- Day06/Reflection/src/main/java/com/reflectionbasicquestion/SimpleReflection.java
+++ Day06/Reflection/src/main/java/com/reflectionbasicquestion/SimpleReflection.java
@@ -1,4 +1,4 @@
-package com;
+package com.reflectionbasicquestion;
 
 import java.lang.reflect.*;
 import java.util.Scanner;
@@ -42,4 +42,5 @@ public class SimpleReflection {
         }
     }
 }
+
 
